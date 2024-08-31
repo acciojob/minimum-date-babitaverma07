1328f455f9fe9ac3f5aea2589365a8564e00183c
Babita Verma <147910203+babitaverma07@users.noreply.github.com>
2024-08-31 22:19:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,28 @@
 function minDate(dates) {
-  //write you code here
+  // Initialize the minimum date to the first date in the array
+  let minDate = dates[0];
+
+  // Loop through each date in the array
+  for (let i = 1; i < dates.length; i++) {
+    // Split the current date and the minimum date into year, month, and day components
+    let currentDateParts = dates[i].split("/");
+    let minDateParts = minDate.split("/");
+
+    // Compare the year components
+    if (currentDateParts[0] < minDateParts[0]) {
+      minDate = dates[i];
+    } else if (currentDateParts[0] === minDateParts[0]) {
+      // Compare the month components
+      if (currentDateParts[1] < minDateParts[1]) {
+        minDate = dates[i];
+      } else if (currentDateParts[1] === minDateParts[1]) {
+        // Compare the day components
+        if (currentDateParts[2] < minDateParts[2]) {
+          minDate = dates[i];
+        }
+      }
+    }
+  }
+
+  return minDate;
 }
-
-// Do not change the code
-
-var dates = [
-  "2023/03/01",
-  "2023/03/02",
-  "2023/03/03",
-  "2023/03/04",
-  "2023/03/05",
-  "2023/03/06",
-  "2023/03/07",
-  "2023/03/08",
-  "2023/03/09",
-  "2023/03/10",
-  "2023/03/11",
-  "2023/03/12",
-  "2023/03/13",
-  "2023/03/14",
-  "2023/03/15",
-  "2023/03/16",
-  "2023/03/17",
-  "2023/03/18",
-  "2023/03/19",
-  "2023/03/20",
-  "2023/03/21",
-  "2023/03/22",
-  "2023/03/23",
-  "2023/03/24",
-  "2023/03/25",
-  "2023/03/26",
-  "2023/03/27",
-  "2023/03/28",
-  "2023/03/29",
-  "2023/03/30",
-];
-
-alert(minDate(dates));
